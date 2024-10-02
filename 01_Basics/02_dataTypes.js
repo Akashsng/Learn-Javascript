c5a5d5b66c21705b796c4eb074b594151683bbe6
Akash Singh <128843537+Akashsng@users.noreply.github.com>
2024-10-02 11:18:50 +0000

    learn data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version of JS 
+// alert(3 + 3)   // we are using node not browser
+
+// Code readability should be high
+
+
+// DATA TYPES
+ let state;  // undefined
+
+ // number  => 2^53 range
+ // bigInt  => for large numbers
+ // string = ""
+ // boolean = true / false
+ // undefined  = undefined
+ 
+ // null  = its a standalone datatype
+ 
+ // Object
+
+ console.log(typeof null) // object
+ console.log(typeof state) // undefined
+
+ console.log("Aksh")
