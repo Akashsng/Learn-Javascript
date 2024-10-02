0dfee21717915dde42a0baa19ea9de82330e8abd
Akash Singh <128843537+Akashsng@users.noreply.github.com>
2024-10-02 12:29:53 +0000

    learning comparisons

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 3)
+// console.log(2 == 3)
+// console.log(2 >= 3)
+// console.log(2 <= 3)
+// console.log(2 != 3)
+
+console.log("02" > 1);
+console.log(1 > "2");
+
+console.log(null == 0);
+console.log(null >0);
+console.log(null >=0);     // for comparison null is treated as 0
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);  // all are false
+console.log(undefined >= 0);
+
+       // STRICT COMPARISON "==="
+
+       console.log("3" === 3) // false
